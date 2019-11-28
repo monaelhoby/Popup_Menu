bc1018cb4e0177e6a94ee8582fd91c29bb4d38b1
mona mohamed <monaelhoby@yahoo.com>
2019-11-29 00:10:27 +0200

    Updates

---
+++ precache-manifest.fdcadd92a3db4ff86a78061701215b11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e91bc227926e14b0b963f3614198a3",
+    "url": "/monaelhoby/popup_menu/index.html"
+  },
+  {
+    "revision": "f0bcafd7f17357af0ef4",
+    "url": "/monaelhoby/popup_menu/static/css/main.efabee95.chunk.css"
+  },
+  {
+    "revision": "5a56f071dfc82bd846d9",
+    "url": "/monaelhoby/popup_menu/static/js/2.00d4c481.chunk.js"
+  },
+  {
+    "revision": "f0bcafd7f17357af0ef4",
+    "url": "/monaelhoby/popup_menu/static/js/main.692bc4e7.chunk.js"
+  },
+  {
+    "revision": "336ce5da50cf46dda133",
+    "url": "/monaelhoby/popup_menu/static/js/runtime-main.0264facb.js"
+  }
+]);
